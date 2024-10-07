93643569e8403cb71567ddd10d0910d36ebe9cca
Stanislas Duarte <duartestanislas.pro@gmail.com>
2024-10-07 09:16:49 +0200

    model author terminé

--- model/authors.model.php
+++ model/authors.model.php
@@ -3,6 +3,9 @@
  * Composant d'accès à la table authors
  */
 
+require_once __DIR__ . '/pdo.php';
+$pdo = getPdo();
+
 
 /**
 * Retourne la liste des auteurs
@@ -11,21 +14,32 @@
 */
  function getAuthors(PDO $pdo) : array
  {
+    $sql= 'SELECT *, DATE_FORMAT(birthday, "%d/%m/%Y") as naissance_europe, DATE_FORMAT(deathday, "%d/%m/%Y") as deces_europe FROM authors';
+    $q = $pdo->query($sql);
+    return $q->fetchAll();
 
  }
-
+ // test unitaire
+//var_dump(getAuthors($pdo));
+//die;
 
 /**
  * Summary of getOneAuthor
  * @param PDO $pdo connexion à la BDD
  * @param int $id identifiant du tuple
- * @return array
+ * @return array|bool 
  */
-function getOneAuthor(PDO $pdo, int $id) : array
+function getOneAuthor(PDO $pdo, int $id) : array|bool
 {
-
+    $sql= 'SELECT *, DATE_FORMAT(birthday, "%d/%m/%Y") as naissance_europe, DATE_FORMAT(deathday, "%d/%m/%Y") as deces_europe FROM authors WHERE id=?';
+    $q = $pdo->prepare($sql);
+    $q->execute([$id]);
+    return $q->fetch();
 }
 
+/*var_dump(getOneAuthor($pdo, 1));
+var_dump(getOneAuthor($pdo, 7));
+die;*/
 
 /**
  * Crée un utilisateur
@@ -33,11 +47,28 @@ function getOneAuthor(PDO $pdo, int $id) : array
  * @param array $data données de l'utilisateur
  * @return int identfiant du nouvel utilisateur
  */
-function createAuthor(PDO $pdo, array $data) : int
+function createAuthor(PDO $pdo, array $data) : int|false
 {
-
+    $sql='INSERT INTO authors(author,biography,birthday,deathday,src) VALUES (:author,:bio,:birthday,:deathday,:src)';
+    $q = $pdo->prepare($sql);
+    $q->bindValue(':author', $data['author']);
+    $q->bindValue(':bio', $data['bio']);
+    $q->bindValue(':birthday', $data['birthday']);
+    $q->bindValue(':deathday', $data['deathday']);
+    $q->bindValue(':src', $data['src']);
+    if ($q->execute()){
+        return $pdo->lastInsertId();
+    }else {return false;}
+    
 }
-
+//test unitiare
+/*var_dump(createAuthor($pdo,[
+'author'=>'Joana',
+'bio'=>'elle démonte des pneus',
+'birthday'=>'1988-09-09',
+'deathday'=>null,
+'src'=>'https://joana.lesacteursduweb.fr/images/imgjo.jpg'
+]));*/
 
 /**
  * Modifie un auteur
@@ -46,11 +77,34 @@ function createAuthor(PDO $pdo, array $data) : int
  * @param int $id identifiant de l'utilisateur
  * @return array le tuple modifié
  */
-function updateAuthor(PDO $pdo, array $data, int $id) : array
+function updateAuthor(PDO $pdo, array $data, int $id) : array|false
 {
+    $sql='UPDATE authors SET 
+    author=:author,
+    biography=:bio,
+    birthday=:birthday,
+    deathday=:deathday,
+    src=:src WHERE id=:id';
+    $q =$pdo->prepare($sql);
+    $q->bindValue(':author',$data['author']);
+    $q->bindValue(':src', $data['src']);
+    $q->bindValue(':bio', $data['bio']);
+    $q->bindValue(':birthday', $data['birthday']);
+    $q->bindValue(':deathday', $data['deathday']);
+    $q->bindValue(':id',$id, PDO::PARAM_INT);
+    if($q->execute()){
+        // on retourne l'auteur
+        return getOneAuthor($pdo,$id);
+    }else {return false;}
 
 }
-
+var_dump(updateAuthor($pdo,[
+    'author'=>'JoanaLaffitte',
+    'bio'=>'elle démonte ses adversaires',
+    'birthday'=>'1988-09-09',
+    'deathday'=>null,
+    'src'=>'https://joana.lesacteursduweb.fr/images/imgjo.jpg'
+    ],9));
 
 /**
  * Supprime un utilisateur
@@ -59,5 +113,18 @@ function updateAuthor(PDO $pdo, array $data, int $id) : array
  * @return bool
  */
 function deleteAuthor(PDO $pdo, int $id) : bool{
-
+    $sql='SELECT COUNT(author) FROM authors WHERE id=?';
+    $q = $pdo->prepare($sql);
+    $q->execute([$id]);
+    if ($q->fetchColumn() == 0){
+        return false;
+    } 
+    
+    $sql='DELETE FROM authors WHERE id=?';
+    $q = $pdo->prepare($sql);
+    return $q->execute([$id]);
 }
+//test unitaire
+//var_dump(deleteAuthor($pdo,3));
+//var_dump(getOneAuthor($pdo,3));
+//var_dump (getAuthors($pdo));
