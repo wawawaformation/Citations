33f11ef1ed036601adb773921dbfa1ce5ead2fd0
Ying <zhangying5810pro@gmail.com>
2024-10-09 14:51:05 +0200

    conflit3

--- view/layout.view.php
+++ view/layout.view.php
@@ -8,7 +8,7 @@
     <title><?= $title ?>Citations</title>
     <link rel="shortcut icon"
         href="https://st.depositphotos.com/1139310/4070/i/450/depositphotos_40709279-stock-photo-3d-illustration-of-chutting-icon.jpg"
-        type="image/x-icon">
+        type="image/jpeg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
