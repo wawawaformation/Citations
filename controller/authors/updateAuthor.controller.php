636190d1ee76c8e6e838f85ac4d365b8f509a983
Ying <zhangying5810pro@gmail.com>
2024-10-10 14:51:21 +0200

    fix update et fix allview

--- controller/authors/updateAuthor.controller.php
+++ controller/authors/updateAuthor.controller.php
@@ -3,33 +3,46 @@
 if(!isset($_GET['id'])){
     $_SESSION['msg'] = [
         'code'=> 'warning',
-        'text'=> 'L\'auteur est untrouvable.'
+        'text'=> 'L\'auteur est introuvable.'
     ];
     header('Location: index.php?controller=authors');
 }
 
-/*
-echo '<pre>';
-print_r($_POST);
-print_r($_GET['id']);
-echo '</pre>';
-die();
-*/
 
-if (isset($_POST['author'], $_POST['birthday'], $_POST['deathday'], $_POST['biography'], $_FILES['src'])) {
+if(isset($_POST['author'], $_POST['birthday'], $_POST['deathday'], $_POST['biography'], $_POST['garder_img'])){
+    $src = getOneAuthor($pdo, $_GET['id'])['src'];
+    if($_POST['garder_img'] == 'non'){
+        $path = ROOT . '/public/' . $src;
+        unlink($path);
+        $src = null;
+    }
 
-   if(updateAuthor($pdo, [
-        'author'=> $_POST['author'],
-        'birthday'=> $_POST['birthday'],
-        'deathday'=> $_POST['deathday'],
-        'biography'=> $_POST['biography'],
-        'src'=> $_FILES['src']
-    ], 
-    $_GET['id'])){
 
-    };
+
+
+    if($_FILES['src'] && $_FILES['src']['error']==0){
+        $path = ROOT . '/public/' . $src;
+        unlink($path);
+
+        $from = $_FILES['src']['tmp_name'];
+        $to = ROOT . '/public/images/authors/' . $_FILES['src']['name'];
+        $src = 'images/authors/' . $_FILES['src']['name'];
+        move_uploaded_file($from, $to);
+    }
+    updateAuthor($pdo, [
+        'author'=>$_POST['author'],
+        'birthday'=>$_POST['birthday'],
+        'deathday'=>$_POST['deathday'],
+        'biography'=>$_POST['biography'],
+        'src'=>$src
+    ], $_GET['id']);
+    $_SESSION['msg'] = [
+        'code'=> 'succes',
+        'text'=> 'L\'image a été modifiée'
+    ];
     header('Location: index.php?controller=authors');
 }
+
 
 
 $author = getOneAuthor($pdo, $_GET['id']);
