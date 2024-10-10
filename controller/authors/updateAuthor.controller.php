4a694e49a9b9cdf4dfb48ffe3021fd5f69078367
David LEGRAND <wawawaformation@gmail.com>
2024-10-10 17:33:53 +0200

    fix merge conflit in updateAuthorController

--- controller/authors/updateAuthor.controller.php
+++ controller/authors/updateAuthor.controller.php
@@ -9,11 +9,7 @@ if(!isset($_GET['id'])){
 }
 
 
-<<<<<<< HEAD
-if(isset($_POST['author'], $_POST['biography'], $_POST['birthday'], $_POST['deathday'], $_POST['garder_img'])){
-=======
 if(isset($_POST['author'], $_POST['birthday'], $_POST['deathday'], $_POST['biography'], $_POST['garder_img'])){
->>>>>>> e4bacbbc7979749e49f3ea312fa364e4c139977f
     $src = getOneAuthor($pdo, $_GET['id'])['src'];
     if($_POST['garder_img'] == 'non'){
         $path = ROOT . '/public/' . $src;
@@ -21,28 +17,6 @@ if(isset($_POST['author'], $_POST['birthday'], $_POST['deathday'], $_POST['biogr
         $src = null;
     }
 
-<<<<<<< HEAD
-    if(isset($_FILES['src']) && !$_FILES['src']['error']){
-        $path = ROOT . '/public/' . $src;
-        unlink($path);
-       
-        $tmp = $_FILES['src']['tmp_name'];
-
-        $src = 'images/authors/' . $_FILES['src']['name'];
-        $path = ROOT . '/public/' .$src;
-        move_uploaded_file($tmp, $path);
-    }
-    updateAuthor($pdo, [
-        'author'=>$_POST['author'],
-        'biography'=>$_POST['biography'],
-        'birthday' =>$_POST['birthday'],
-        'deathday' => $_POST['deathday'],
-        'src' => $src
-    ], $_GET['id']);
-    $_SESSION['msg'] = [
-        'code'=> 'success',
-        'text'=> 'L\'auteur a bien été modifié'
-=======
 
 
 
@@ -65,17 +39,12 @@ if(isset($_POST['author'], $_POST['birthday'], $_POST['deathday'], $_POST['biogr
     $_SESSION['msg'] = [
         'code'=> 'succes',
         'text'=> 'L\'image a été modifiée'
->>>>>>> e4bacbbc7979749e49f3ea312fa364e4c139977f
     ];
     header('Location: index.php?controller=authors');
 }
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> e4bacbbc7979749e49f3ea312fa364e4c139977f
 $author = getOneAuthor($pdo, $_GET['id']);
 
 require_once ROOT . '/view/template/authors/update.view.php';
