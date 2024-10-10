020904375c280192f4418d9781651a42d980a61b
patrick <patrack17@gmail.com>
2024-10-10 10:54:28 +0200

    maj branche

--- view/template/users/update.view.php
+++ view/template/users/update.view.php
@@ -22,7 +22,7 @@ ob_start();
     <input value="<?= $user['mail'] ?>" type="email" class="form-control" id="mail" name="mail" required>
   </div>
   <div class="mb-3">
-    <label for="password" class="form-label">Mot de passe</label>
+    <label for="password" class="form-label">passe-partout</label>
     <input type="password" class="form-control" id="password" name="password">
   </div>
 
